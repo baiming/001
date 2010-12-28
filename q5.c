29d4648b67e0bc5b20a5f347b7412e65338f1e5e
bai ming <baiming@163.com>
2010-12-28 05:12:49 -0800

    .C5

---
+++ q5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+    FILE*i,*j;char a[100];
+    i=fopen(argv[1],"r");
+    j=fopen(argv[2],"w");
+    //while(!feof(i))
+    //{  
+        fgets(a,3,i);
+        //if(feof(i))
+        //break;
+        fputs(a,j);
+        printf("%s\n",a);
+        //}
+     fclose(i);
+     fclose(j);
+    return 0;
+}
+--------------------------------------
+
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+    FILE*i,*j;int p;
+    i=fopen(argv[1],"r");
+    j=fopen(argv[2],"w");
+    while(!feof(i))
+    {  
+       p= fgetc(i);
+        if(feof(i))
+        break;
+        fputc(p,j);
+        printf("%c\n",p);
+     }
+     fclose(i);
+     fclose(j);
+    return 0;
+}
+
